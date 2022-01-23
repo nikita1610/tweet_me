9a1e337378e000507da93394b381043f6f188ffb
nikita1610 <jainnikita381@gmail.com>
2022-01-23 22:45:08 +0530

    Tweets Models

--- tweets/models.py
+++ tweets/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 
 class Tweet(models.Model):
-    content = models.TextField()
-    image   = models.FileField(upload_to='images/')
+    content = models.TextField(blank=True,null=True)
+    image   = models.FileField(upload_to='images/',blank=True,null=True)
