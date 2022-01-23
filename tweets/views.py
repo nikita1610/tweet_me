723a587cd80b57b073899bb003ae4c9139dd976e
nikita1610 <jainnikita381@gmail.com>
2022-01-23 23:10:33 +0530

    Tweets Models

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse,Http404
 # Create your views here.
+
+from .models import Tweet
+
+def home_view(request,*args,**kwargs):
+    print(args,kwargs)
+    return HttpResponse("<h1>Hello World </h1>")
+
+def tweet_detail_view(request,tweet_id,*args,**kwargs):
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+    except Exception as e:
+        raise Http404
+    return HttpResponse(f"<h1>Hello {tweet_id} - {obj.content}</h1>")
